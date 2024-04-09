ec7296516ecbe2670c9cbe6b96521c782da1ce12
JIANHONG OU <jianhong@users.noreply.github.com>
2024-04-09 09:26:01 -0400

    remove the sample limitation for plotTE. add a new functon normBy.

---
+++ R/normBy.R
@@ -0,0 +1,63 @@
+#' Normalization by edgeR, DESeq2 or RUVSeq
+#' @description Normalization by multiple known methods
+#' @param counts Output of \link{countReads}
+#' @param method Character(1L) to indicate the method for normalization.
+#' @param ... parameters will be passed to \link{normByRUVs}
+#' @return Normalized counts list
+#' @export
+#' @examples
+#' path <- system.file("extdata", package="ribosomeProfilingQC")
+#' cnts <- readRDS(file.path(path, "cnts.rds"))
+#' norm <- normBy(cnts, method = 'edgeR')
+#' norm2 <- normBy(cnts, method = 'DESeq2')
+#' 
+normBy <- function(counts, method = c('edgeR', 'DESeq2', 'RUVs'), ...){
+  if(!any(c("RPFs", "mRNA") %in% names(counts))){
+    stop("counts must be output of coutReads.")
+  }
+  method <- match.arg(method)
+  switch(method,
+         edgeR = normByDETools(counts, FUN=edgeRnormHelper),
+         DESEq2 = normByDETools(counts, FUN=DESeq2normHelper),
+         RUVs = normByRUVs(counts, ...))
+}
+
+normByDETools <- function(count, FUN){
+  stopifnot(is.function(FUN))
+  if("RPFs" %in% names(counts)){
+    counts$RPFsRawCounts <- counts$RPFs
+    RPFs <- counts$RPFs
+    counts$RPFs <- FUN(RPFs)
+  }
+  
+  if("mRNA" %in% names(counts)){
+    counts$mRNARawCounts <- counts$mRNA
+    mRNA <- counts$mRNA
+    counts$mRNA <- FUN(mRNA)
+  }
+  counts
+}
+
+edgeRnormHelper <- function(x){
+  if (!requireNamespace("edgeR", quietly=TRUE)) {
+    stop("method='edgeR' requires installing the CRAN package 'edgeR'")
+  }
+  y <- edgeR::DGEList(counts=x)
+  y <- edgeR::normLibSizes(y)
+  cpm <- edgeR::cpm(y, log = TRUE)
+  return(cpm)
+}
+
+DESeq2normHelper <- function(x){
+  if (!requireNamespace("DESeq2", quietly=TRUE)) {
+    stop("method='DESeq2' requires installing the CRAN package 'DESeq2'")
+  }
+  cd <- data.frame(sample=factor(colnames(x)))
+  design <- as.formula('~sample')
+  dds <- DESeq2::DESeqDataSetFromMatrix(countData=x,
+                                        colData = cd,
+                                        design = design)
+  dds <- DESeq2::estimateSizeFactors(dds)
+  cnt <- DESeq2::counts(dds, normalized=TRUE)
+  return(cnt)
+}
--- R/plotTE.R
+++ R/plotTE.R
@@ -2,13 +2,13 @@
 #' @description Scatterplot of RNA/RPFs level compared to the
 #' translational efficiency.
 #' @param TE Output of \link{translationalEfficiency}
-#' @param sample character(1). Sample name to plot.
+#' @param sample Sample names to plot.
 #' @param xaxis What to plot for x-axis.
 #' @param removeZero Remove the 0 values from plots.
 #' @param log2 Do log2 transform or not.
 #' @param type,margins,... Parameters pass to ggMarginal
 #' @return A ggplot object.
-#' @importFrom ggplot2 ggplot geom_point geom_text .data
+#' @importFrom ggplot2 ggplot geom_point geom_text .data facet_wrap
 #' @importFrom ggExtra ggMarginal
 #' @importFrom stats as.formula coef na.omit
 #' @export
@@ -33,58 +33,70 @@ plotTE <- function(TE, sample, xaxis=c("mRNA", "RPFs"),
   if(!any(c("RPFs", "mRNA", "TE") %in% names(TE))){
     stop("TE must be output of translationalEfficiency.")
   }
-  if(missing(sample)){
-    stop("sample is required.")
-  }
   xaxis <- match.arg(xaxis)
   mRNA <- TE$mRNA
   RPFs <- TE$RPFs
   x <- TE[[xaxis]]
   TE <- TE$TE
+  if(missing(sample)){
+    sample <- colnames(TE)
+  }
   if(!is.numeric(sample)){
     sample <- which(colnames(TE) %in% sample)
   }
-  if(length(sample)>1){
-    sample <- sample[1]
-    message("Only first sample will be plotted.")
-  }
-  x <- x[, sample]
-  TE <- TE[, sample]
-  RPFs <- RPFs[, sample]
+  single_sample <- length(sample)==1
+  df <- lapply(sample, function(.sample){
+    data.frame(x = x[, .sample],
+               TE = TE[, .sample],
+               sample = .sample,
+               RPFs = RPFs[, .sample],
+               mRNA = mRNA[, .sample])
+  })
+  
+  df <- do.call(rbind, df)
   if(removeZero){
-    keep <- RPFs>0 & mRNA>0
+    keep <- df$RPFs>0 & df$mRNA>0
     if(sum(keep)<1){
       stop("No data available for plotting.")
     }
-    x <- x[keep]
-    TE <- TE[keep]
+    df <- df[keep, , drop=FALSE]
   }
+  df$sample <- colnames(TE)[df$sample]
   xlab <- paste(xaxis, "level")
   ylab <- "Translational Efficiency"
   if(log2){
-    x <- log2(x)
-    TE <- log2(TE)
+    df$x <- log2(df$x)
+    df$TE <- log2(df$TE)
     xlab <- paste('log2 transformed', xlab)
     ylab <- paste('log2 transformed', ylab)
   }
-  df <- data.frame(x=x, TE=TE)
+
   lm_eqn <- function(df){
     df <- df[!is.infinite(df$x) & !is.infinite(df$TE), , drop=FALSE]
-    m <- lm(as.formula("TE ~ x"), df, na.action = na.omit)
-    eq <- substitute(italic("TE") == a + b %.% italic(x)*","~~italic("r")^2~"="~r2, 
-                     list(a = format(unname(coef(m)[1]), digits = 2),
-                          b = format(unname(coef(m)[2]), digits = 2),
-                          r2 = format(summary(m)$r.squared, digits = 3),
-                          x = xaxis))
-    as.character(as.expression(eq))
+    df <- split(df, df$sample)
+    out <- lapply(df, function(.df){
+      m <- lm(as.formula("TE ~ x"), .df, na.action = na.omit)
+      eq <- substitute(italic("TE") == a + b %.% italic(x)*","~~italic("r")^2~"="~r2, 
+                       list(a = format(unname(coef(m)[1]), digits = 2),
+                            b = format(unname(coef(m)[2]), digits = 2),
+                            r2 = format(summary(m)$r.squared, digits = 3),
+                            x = xaxis))
+      as.character(as.expression(eq))
+    })
+    out <- data.frame(sample=names(out), label=unlist(out))
   }
   p <- ggplot(df, aes(.data$x, .data$TE)) + geom_point() + 
     xlab(xlab) + ylab(ylab) +
     geom_smooth(method="lm", se=TRUE)
-  p <- p + geom_text(data=data.frame(),
-                     aes(x=-Inf, y = Inf, label=lm_eqn(df)),
+  p <- p + geom_text(data=lm_eqn(df),
+                     aes(x=-Inf, y = Inf, label=.data$label),
                      hjust = 0, vjust = 1,
                      parse = TRUE)
-  p <- ggMarginal(p, type=type, margins=margins, ...)
+  if(single_sample){
+    p <- ggMarginal(p, type=type, margins=margins, ...)
+  }else{
+    p <- p +
+      facet_wrap(facets = as.formula('~sample'))
+  }
   p
 }
